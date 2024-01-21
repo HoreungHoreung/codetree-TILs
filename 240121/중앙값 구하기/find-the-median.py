086f41c36d55c1be979250aa79594d12fe6fb21a
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-01-21 23:36:48 +0900

    Title: 중앙값 구하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240121/중앙값 구하기/find-the-median.py
+++ 240121/중앙값 구하기/find-the-median.py
@@ -1,14 +1,2 @@
-a,b,c = tuple(map(int, input().split()))
-
-if a > b and a < c:
-    print(a)
-elif a > b and a > c:
-    if b < c:
-        print(b)
-    else:
-        print(c)
-else:
-    if b < c:
-        print(b)
-    else:
-        print(c)
+a = list(map(int, input().split()))
+print(sorted(a)[1])
