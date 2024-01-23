85d067068bc7ba456e7426e4a28e07cdf07fe925
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-01-23 15:41:03 +0900

    Title: 방향에 맞춰 이동, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/방향에 맞춰 이동/move-in-direction.py
+++ 240123/방향에 맞춰 이동/move-in-direction.py
@@ -5,11 +5,13 @@ for _ in range(N):
     direction, distance = input().split()
     distance = int(distance)
     if (direction == 'E'):
-        nx, ny = nx + (distance * dx[0]), ny + (distance * dy[0])
+        direction = 0
     elif (direction == 'S'):
-        nx, ny = nx + (distance * dx[1]), ny + (distance * dy[1])
+        direction = 1
     elif (direction == 'W'):
-        nx, ny = nx + (distance * dx[2]), ny + (distance * dy[2])
+        direction = 2
     else:
-        nx, ny = nx + (distance * dx[3]), ny + (distance * dy[3])
+        direction = 3
+    nx += dx[direction] * distance
+    ny += dy[direction] * distance
 print(nx, ny)
