420adb0516fe2c96c07cfab22e4f2d4173be6097
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-01-24 22:23:51 +0900

    Title: 1이 3개 이상 있는 위치, Time: 201ms, Memory: 26MB, Status: Passed - CodeTree

--- 240124/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240124/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,9 +1,9 @@
 n = int(input())
-a = [
-    list(map(int, input().split()))
-    for _ in range(n)]
-
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+arr = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
@@ -12,12 +12,12 @@ def adjacent_cnt(x, y):
     cnt = 0
     for dx, dy in zip(dxs, dys):
         nx, ny = x + dx, y + dy
-        if in_range(nx, ny) and a[nx][ny] == 1:
+        if in_range(nx, ny) and arr[nx][ny] == 1:
             cnt += 1
-        
-    return cnt
+    return(cnt)
 
-ans = 0
+ans = 0    
+
 for i in range(n):
     for j in range(n):
         if adjacent_cnt(i,j) >= 3:
