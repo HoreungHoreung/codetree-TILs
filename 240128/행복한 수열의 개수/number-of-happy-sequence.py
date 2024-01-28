f05c6b98753590e70166023bd279825d251c4f23
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-01-28 23:45:30 +0900

    Title: 행복한 수열의 개수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240128/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,42 @@
+n, m = tuple(map(int, input().split()))
+
+grid = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+def is_happy_row(row):
+    consecutive = 1
+    for i in range(1, n):
+        if grid[row][i - 1] == grid[row][i]:
+            consecutive += 1
+        else:
+            consecutive = 1
+        
+        if consecutive >= m:
+            return True
+
+def is_happy_col(col):
+    consecutive = 1
+    for i in range(1, n):
+        if grid[i - 1][col] == grid[i][col]:
+            consecutive += 1
+        else:
+            consecutive = 1
+
+        if consecutive >= m:
+            return True 
+
+        
+
+
+happy_sequence = 0
+for row in range(n):
+    if is_happy_row(row):
+        happy_sequence += 1
+
+for col in range(n):
+    if is_happy_col(col):
+        happy_sequence += 1
+
+print(happy_sequence)
